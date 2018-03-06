236ca0e494b72b5c4e3d608136cf7dd91ab0eaf0
nebbsie <aaron@nebbs.com>
2018-03-06 15:46:00 +0000

    Added image getter helper.

---
+++ app/src/main/java/com/aaronnebbs/peersplitandroidapplication/Helpers/ImageSelector.java
@@ -0,0 +1,12 @@
+package com.aaronnebbs.peersplitandroidapplication.Helpers;
+
+import com.aaronnebbs.peersplitandroidapplication.R;
+
+
+public class ImageSelector {
+
+    public static int getTypeImage(String str){
+        return R.drawable.emaillogo;
+    }
+
+}
