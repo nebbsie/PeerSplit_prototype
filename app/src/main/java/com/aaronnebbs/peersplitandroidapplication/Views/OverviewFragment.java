4fc1d170131943b40bbeb7d13a4f75a385b271df
nebbsie <aaron@nebbs.com>
2018-03-11 23:47:58 +0000

    Added new images,

--- app/src/main/java/com/aaronnebbs/peersplitandroidapplication/Views/OverviewFragment.java
+++ app/src/main/java/com/aaronnebbs/peersplitandroidapplication/Views/OverviewFragment.java
@@ -1,14 +1,19 @@
 package com.aaronnebbs.peersplitandroidapplication.Views;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.aaronnebbs.peersplitandroidapplication.R;
 
 public class OverviewFragment extends Fragment {
+
 
     public OverviewFragment(){
 
@@ -19,9 +24,18 @@ public class OverviewFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.overview_fragment, container, false);
+
     }
 
     public static OverviewFragment newInstance() {
